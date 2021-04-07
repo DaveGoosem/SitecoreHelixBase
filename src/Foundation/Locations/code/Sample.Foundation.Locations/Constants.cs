c730cf2c25509718df192b22cd4cb1af538d497c
David Goosem <dgoosem@gmail.com>
2021-04-07 22:22:36 +1000

    Update Constants.cs

--- src/Foundation/Locations/code/Sample.Foundation.Locations/Constants.cs
+++ src/Foundation/Locations/code/Sample.Foundation.Locations/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string LocationsIndexName = "g8_locations_index_web";
+        public const string LocationsIndexName = "sample_locations_index_web";
 
         public const string LocationsItemBucketGuid = "{D537EA73-95EC-49E1-991A-11EF8A998435}";
         public const string LocationsItemTemplateGuid = "{D7CCA97C-60F3-49B0-A3A3-4E5E521918E9}";
